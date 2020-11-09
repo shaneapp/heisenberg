f88d04c28713d8b1ba43a6063c0a07c409ffa740
shaneapp <shaneapp@hotmail.com>
2020-11-09 17:30:41 +0000

    add instrumentation tests

--- app/src/androidTest/java/com/appleby/breakingbad/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/appleby/breakingbad/ExampleInstrumentedTest.kt
@@ -1,12 +1,17 @@
 package com.appleby.breakingbad
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.mockk.*
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +20,46 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.appleby.breakingbad", appContext.packageName)
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Rule
+    @JvmField
+    val schedulers = RxImmediateSchedulerRule()
+
+    lateinit var myViewModel: CharacterListViewModel
+    private val observer: Observer<CharacterStates> = mockk(relaxed = true)
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        mockkObject(CharacterRepo)
+        myViewModel = CharacterListViewModel()
+        myViewModel.result.observeForever(observer)
     }
+
+    @Test
+    fun seasonFilterEmitsCorrectState() {
+        myViewModel.refreshSeasonFilter(2)
+        val state = myViewModel.result.value
+        assertEquals(CharacterStates.SeasonFilter(2) ,state)
+        verify(exactly = 1) { observer.onChanged(any()) }
+    }
+
+    @Test
+    fun seasonFilterRefreshesCorrectly() {
+        myViewModel.refreshSeasonFilter(3)
+        myViewModel.requestFilterRefresh()
+        assertEquals(CharacterStates.SeasonFilter(3), myViewModel.result.value)
+        verify(exactly = 2) { observer.onChanged(any()) }
+    }
+
+    @Test
+    fun characterRequestSuccess() {
+        myViewModel.requestCharacters()
+        every { CharacterRepo.characters } returns mutableListOf()
+        assert(myViewModel.result.value is CharacterStates.NetworkSuccess)
+    }
+
 }
