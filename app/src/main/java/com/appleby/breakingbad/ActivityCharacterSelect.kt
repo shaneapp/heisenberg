a09c1f3b4063ca955af5eb0f3c422167cae010fe
shaneapp <shaneapp@hotmail.com>
2020-11-09 18:22:58 +0000

    consume livedata

--- app/src/main/java/com/appleby/breakingbad/ActivityCharacterSelect.kt
+++ app/src/main/java/com/appleby/breakingbad/ActivityCharacterSelect.kt
@@ -2,6 +2,7 @@ package com.appleby.breakingbad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
@@ -18,14 +19,26 @@ class ActivityCharacterSelect : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewModel.result.observe(this, Observer {
-
+            when(it) {
+                is CharacterStates.NetworkSuccess -> updateRecyclerView(it.season)
+                is CharacterStates.SeasonFilter -> updateRecyclerView(it.season)
+                is CharacterStates.NetworkFailure -> {
+                    Toast.makeText(this, "Request failed", Toast.LENGTH_SHORT).show()
+                }
+            }
         })
 
         rvCharacterList.layoutManager = GridLayoutManager(this, 2)
         rvCharacterList.adapter = characterListAdapter
     }
 
-    private fun updateRecyclerView() {
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.requestCharacters()
+    }
+
+    private fun updateRecyclerView(season: Int) {
         characterListAdapter.updateData(CharacterRepo.characters)
     }
 
