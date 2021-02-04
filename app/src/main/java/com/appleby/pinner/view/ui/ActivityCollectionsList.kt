43101ada5488f3b0ec3290317663ddad3328a187
shaneapp <shaneapp@hotmail.com>
2021-02-04 18:17:57 +0000

    Add button to download folder

--- app/src/main/java/com/appleby/pinner/view/ui/ActivityCollectionsList.kt
+++ app/src/main/java/com/appleby/pinner/view/ui/ActivityCollectionsList.kt
@@ -1,6 +1,9 @@
 package com.appleby.pinner.view.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.appleby.pinner.CustomDialogs
@@ -12,6 +15,7 @@ import kotlinx.android.synthetic.main.activity_collections.*
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
 import java.util.*
+
 
 class ActivityCollectionsList : AppCompatActivity() {
 
@@ -30,6 +34,20 @@ class ActivityCollectionsList : AppCompatActivity() {
                 ObjectBox.collectionBox.put(newCollection)
                 refreshCollectionList()
 
+            }
+        }
+
+        ivDownloadFolder.setOnClickListener {
+            val downloadDir = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.path
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setDataAndType(Uri.parse(downloadDir), "resource/folder")
+
+            if (intent.resolveActivityInfo(packageManager, 0) != null) {
+                startActivity(intent)
+            } else {
+                // if you reach this place, it means there is no any file
+                // explorer app installed on your device
             }
         }
 
